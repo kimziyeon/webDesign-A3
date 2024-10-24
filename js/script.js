42145dbfb74ef97ddac5ca6e6ac6c90ed2e01460
kimziyeon <aa40254037@gmail.com>
2024-10-24 12:29:34 +0900

    20241024

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 $(function () {
+
+    // 팝업
+
     $('.popup_on').click(function () {
         $('.popup').show();
     })
@@ -6,6 +9,8 @@ $(function () {
     $('.x').click(function () {
         $('.popup').hide();
     })
+
+    //탭메뉴
 
     $('.tab>li').click(function () {
         $('.tab>li').removeClass('on');
@@ -16,4 +21,39 @@ $(function () {
         $('.tabcon').hide();
         $('.tabcon').eq(i).show();
     })
+
+    //헤더메뉴
+
+    $('nav>ul>li').mouseenter(function () {
+
+        var i = $(this).index();
+        // console.log('index', i);
+        $('.sub').slideUp();
+        $('.sub').eq(i).stop().slideDown();
+
+        $('nav>ul>li').removeClass('on');
+        $('nav>ul>li').eq(i).addClass('on');
+    })
+
+    $('nav>ul>li').mouseleave(function () {
+        $('.sub').stop().slideUp();
+        $('nav>ul>li').removeClass('on');
+    })
+
+    // 슬라이드
+
+    var s = 0;
+
+    function slide() {
+
+        if (s < 2) {
+            s++;
+        } else {
+            s = 0;
+        }
+        $('.slide ul li').fadeOut();
+        $('.slide ul li').eq(s).fadeIn();
+    }
+    setInterval(slide, 3000)
+
 })
